0803bfac0996314444d4c7ced301eeb7af59a7b0
Paul Avila <avila_pa@yahoo.com>
2023-03-21 12:02:01 -0700

    updates

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const cors = require('cors')
 const path = require('path')
 
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// server.get('/login', (req, res) => {
+//   res.jsonp(req.query)
+// })
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
-server.use(middlewares)
 server.use(router)
 
 const PORT = 8000
